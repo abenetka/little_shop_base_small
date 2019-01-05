b1e7cc42c70fbad44b030a97a6b4ba208fe30a79
abenetka <ali.benetka@gmail.com>
2019-01-05 14:56:27 -0700

    Adds implementation of merchants who fulfilled orders fastest this month

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -16,7 +16,8 @@ class MerchantsController < ApplicationController
     @top_3_quantity_orders = Order.top_3_quantity_orders
     @top_10_merchants_items_sold_this_month = User.top_merchants_items_sold_this_month(10)
     @top_10_merchants_items_sold_last_month = User.top_merchants_items_sold_last_month(10)
-    @top_5_merchants_fulfilled_orders_state = @user.top_merchants_fulfilled_orders_state(5)
+    @top_10_merchants_fulfilled_orders_this_month = User.top_merchants_fulfilled_orders_this_month(10)
+    # @top_5_merchants_fulfilled_orders_state = @user.top_merchants_fulfilled_orders_state(5)
   end
 
   def show
