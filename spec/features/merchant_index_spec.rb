3c5cc6ba38c5e749b796fbd0a801f2a970f6ac65
abenetka <ali.benetka@gmail.com>
2019-01-05 14:55:37 -0700

    Adds tests for merchants that fulfilled orders fastest this month

--- spec/features/merchant_index_spec.rb
+++ spec/features/merchant_index_spec.rb
@@ -342,21 +342,241 @@ NYC, Seattle WA, Seattle FL
         end
       end
     end
+  end
 
-    xit 'shows top 10 merchants who fulfilled non-cancelled orders this month' do
+  describe 'merchant leaderboard top merchants for month' do
+    before :each do
+      @user_1 = create(:user, city: 'Denver', state: 'CO')
+      @user_2 = create(:user, city: 'NYC', state: 'NY')
+      @user_3 = create(:user, city: 'Seattle', state: 'WA')
+      @user_4 = create(:user, city: 'Seattle', state: 'FL')
+
+      @merchant_1 = create(:merchant, name: 'Merchant Name 1')
+      @merchant_2 = create(:merchant, name: 'Merchant Name 2')
+      @merchant_3 = create(:merchant, name: 'Merchant Name 3')
+      @merchant_4 = create(:merchant, name: 'Merchant Name 4')
+      @merchant_5 = create(:merchant, name: 'Merchant Name 5')
+      @merchant_6 = create(:merchant, name: 'Merchant Name 6')
+      @merchant_7 = create(:merchant, name: 'Merchant Name 7')
+      @merchant_8 = create(:merchant, name: 'Merchant Name 8')
+      @merchant_9 = create(:merchant, name: 'Merchant Name 9')
+      @merchant_10 = create(:merchant, name: 'Merchant Name 10')
+
+      @item_1 = create(:item, user: @merchant_1)
+      @item_2 = create(:item, user: @merchant_2)
+      @item_3 = create(:item, user: @merchant_3)
+      @item_4 = create(:item, user: @merchant_4)
+      @item_5 = create(:item, user: @merchant_5)
+      @item_6 = create(:item, user: @merchant_6)
+      @item_7 = create(:item, user: @merchant_7)
+      @item_8 = create(:item, user: @merchant_8)
+      @item_9 = create(:item, user: @merchant_9)
+      @item_10 = create(:item, user: @merchant_10)
+
+      @order_1 = create(:completed_order, user: @user_1)
+      @oi_1 = create(:fulfilled_order_item, item: @item_1, order: @order_1, quantity: 10, price: 11, created_at: 10.minutes.ago, updated_at: 9.minute.ago)
+
+      @order_2 = create(:completed_order, user: @user_2)
+      @oi_2 = create(:fulfilled_order_item, item: @item_1, order: @order_2, quantity: 20, price: 12, created_at: 2.days.ago, updated_at: 1.minute.ago)
+
+      @order_3 = create(:completed_order, user: @user_3)
+      @oi_3 = create(:fulfilled_order_item, item: @item_1, order: @order_3, quantity: 30, price: 13, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_4 = create(:completed_order, user: @user_4)
+      @oi_4 = create(:fulfilled_order_item, item: @item_1, order: @order_4, quantity: 40, price: 14, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_5 = create(:completed_order, user: @user_1)
+      @oi_5 = create(:fulfilled_order_item, item: @item_1, order: @order_5, quantity: 50, price: 15, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_6 = create(:completed_order, user: @user_2)
+      @oi_6 = create(:fulfilled_order_item, item: @item_1, order: @order_6, quantity: 60, price: 16, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_7 = create(:completed_order, user: @user_3)
+      @oi_7 = create(:fulfilled_order_item, item: @item_1, order: @order_7, quantity: 70, price: 17, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_8 = create(:completed_order, user: @user_4)
+      @oi_8 = create(:fulfilled_order_item, item: @item_1, order: @order_8, quantity: 80, price: 18, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_9 = create(:completed_order, user: @user_1)
+      @oi_9 = create(:fulfilled_order_item, item: @item_1, order: @order_9, quantity: 90, price: 19, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+      @order_10 = create(:completed_order, user: @user_2)
+      @oi_10 = create(:fulfilled_order_item, item: @item_1, order: @order_10, quantity: 100, price: 20, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+
+
+      @order_11 = create(:completed_order, user: @user_3)
+      @oi_11 = create(:fulfilled_order_item, item: @item_2, order: @order_11, quantity: 200, price: 11, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_12 = create(:completed_order, user: @user_4)
+      @oi_12 = create(:fulfilled_order_item, item: @item_2, order: @order_12, quantity: 190, price: 12, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_13 = create(:completed_order, user: @user_1)
+      @oi_13 = create(:fulfilled_order_item, item: @item_2, order: @order_13, quantity: 180, price: 13, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_14 = create(:completed_order, user: @user_2)
+      @oi_14 = create(:fulfilled_order_item, item: @item_2, order: @order_14, quantity: 170, price: 14, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_15 = create(:completed_order, user: @user_3)
+      @oi_15 = create(:fulfilled_order_item, item: @item_2, order: @order_15, quantity: 160, price: 15, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_16 = create(:completed_order, user: @user_4)
+      @oi_16 = create(:fulfilled_order_item, item: @item_2, order: @order_16, quantity: 150, price: 16, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_17 = create(:completed_order, user: @user_1)
+      @oi_17 = create(:fulfilled_order_item, item: @item_2, order: @order_17, quantity: 140, price: 17, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_18 = create(:completed_order, user: @user_2)
+      @oi_18 = create(:fulfilled_order_item, item: @item_2, order: @order_18, quantity: 130, price: 18, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_19 = create(:completed_order, user: @user_3)
+      @oi_19 = create(:fulfilled_order_item, item: @item_2, order: @order_19, quantity: 120, price: 19, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_20 = create(:completed_order, user: @user_4)
+      @oi_20 = create(:fulfilled_order_item, item: @item_3, order: @order_20, quantity: 110, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_21 = create(:completed_order, user: @user_1)
+      @oi_21 = create(:fulfilled_order_item, item: @item_3, order: @order_21, quantity: 2, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_22 = create(:completed_order, user: @user_2)
+      @oi_22 = create(:fulfilled_order_item, item: @item_3, order: @order_22, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_23 = create(:completed_order, user: @user_2)
+      @oi_23 = create(:fulfilled_order_item, item: @item_3, order: @order_23, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_24 = create(:completed_order, user: @user_2)
+      @oi_24 = create(:fulfilled_order_item, item: @item_3, order: @order_24, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_25 = create(:completed_order, user: @user_2)
+      @oi_25 = create(:fulfilled_order_item, item: @item_3, order: @order_25, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_26 = create(:completed_order, user: @user_2)
+      @oi_26 = create(:fulfilled_order_item, item: @item_3, order: @order_26, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_27 = create(:completed_order, user: @user_2)
+      @oi_27 = create(:fulfilled_order_item, item: @item_3, order: @order_27, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_28 = create(:completed_order, user: @user_2)
+      @oi_28 = create(:fulfilled_order_item, item: @item_4, order: @order_28, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_29 = create(:completed_order, user: @user_2)
+      @oi_29 = create(:fulfilled_order_item, item: @item_4, order: @order_29, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_30 = create(:completed_order, user: @user_2)
+      @oi_30 = create(:fulfilled_order_item, item: @item_4, order: @order_30, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_31 = create(:completed_order, user: @user_2)
+      @oi_31 = create(:fulfilled_order_item, item: @item_4, order: @order_31, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_32 = create(:completed_order, user: @user_2)
+      @oi_32 = create(:fulfilled_order_item, item: @item_4, order: @order_32, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_33 = create(:completed_order, user: @user_2)
+      @oi_33 = create(:fulfilled_order_item, item: @item_4, order: @order_33, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_34 = create(:completed_order, user: @user_2)
+      @oi_34 = create(:fulfilled_order_item, item: @item_4, order: @order_34, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_36 = create(:completed_order, user: @user_2)
+      @oi_36 = create(:fulfilled_order_item, item: @item_5, order: @order_36, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_37 = create(:completed_order, user: @user_2)
+      @oi_37 = create(:fulfilled_order_item, item: @item_5, order: @order_37, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_38 = create(:completed_order, user: @user_2)
+      @oi_38 = create(:fulfilled_order_item, item: @item_5, order: @order_38, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_39 = create(:completed_order, user: @user_2)
+      @oi_39 = create(:fulfilled_order_item, item: @item_5, order: @order_39, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_40 = create(:completed_order, user: @user_2)
+      @oi_40 = create(:fulfilled_order_item, item: @item_5, order: @order_40, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_41 = create(:completed_order, user: @user_2)
+      @oi_41 = create(:fulfilled_order_item, item: @item_5, order: @order_41, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_42 = create(:completed_order, user: @user_2)
+      @oi_42 = create(:fulfilled_order_item, item: @item_6, order: @order_42, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_43 = create(:completed_order, user: @user_2)
+      @oi_43 = create(:fulfilled_order_item, item: @item_6, order: @order_43, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_44 = create(:completed_order, user: @user_2)
+      @oi_44 = create(:fulfilled_order_item, item: @item_6, order: @order_44, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_45 = create(:completed_order, user: @user_2)
+      @oi_45 = create(:fulfilled_order_item, item: @item_6, order: @order_45, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_46 = create(:completed_order, user: @user_2)
+      @oi_46 = create(:fulfilled_order_item, item: @item_6, order: @order_46, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_47 = create(:completed_order, user: @user_2)
+      @oi_47 = create(:fulfilled_order_item, item: @item_7, order: @order_47, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_48 = create(:completed_order, user: @user_2)
+      @oi_48 = create(:fulfilled_order_item, item: @item_7, order: @order_48, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_49 = create(:completed_order, user: @user_2)
+      @oi_49 = create(:fulfilled_order_item, item: @item_7, order: @order_49, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_50 = create(:completed_order, user: @user_2)
+      @oi_50 = create(:fulfilled_order_item, item: @item_7, order: @order_50, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_51 = create(:completed_order, user: @user_2)
+      @oi_51 = create(:fulfilled_order_item, item: @item_8, order: @order_51, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_52 = create(:completed_order, user: @user_2)
+      @oi_52 = create(:fulfilled_order_item, item: @item_8, order: @order_52, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_53 = create(:completed_order, user: @user_2)
+      @oi_53 = create(:fulfilled_order_item, item: @item_8, order: @order_53, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_54 = create(:completed_order, user: @user_2)
+      @oi_54 = create(:fulfilled_order_item, item: @item_9, order: @order_54, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_55 = create(:completed_order, user: @user_2)
+      @oi_55 = create(:fulfilled_order_item, item: @item_9, order: @order_55, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+      @order_56 = create(:completed_order, user: @user_2)
+      @oi_56 = create(:fulfilled_order_item, item: @item_10, order: @order_56, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      #sad path within time frame, but cancelled
+      @order_57 = create(:cancelled_order, user: @user_2)
+      @oi_57 = create(:fulfilled_order_item, item: @item_1, order: @order_57, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      @order_58 = create(:cancelled_order, user: @user_2)
+      @oi_58 = create(:fulfilled_order_item, item: @item_10, order: @order_58, quantity: 1, price: 20, created_at: 2.days.ago, updated_at: 1.day.ago)
+
+      #sad path cancelled and out of time frame
+      @order_59 = create(:cancelled_order, user: @user_2)
+      @oi_59 = create(:fulfilled_order_item, item: @item_10, order: @order_59, quantity: 1, price: 20, created_at: 3.months.ago, updated_at: 32.days.ago)
+
+      #sad path fulfilled but not in time frame
+      @order_60 = create(:completed_order, user: @user_2)
+      @oi_60 = create(:fulfilled_order_item, item: @item_10, order: @order_60, quantity: 1, price: 20, created_at: 3.months.ago, updated_at: 32.days.ago)
+    end
+
+    it 'shows top 10 merchants who fulfilled non-cancelled orders this month' do
       visit merchants_path
       within '#leaderboard' do
         within '#top-10-merchants-fulfilled-orders-this-month' do
-          # expect(page.all('.merchant')[0]).to have_content('Merchant Name 1, Items Sold: 200')
-          # expect(page.all('.merchant')[1]).to have_content('Merchant Name 2, Items Sold: 190')
-          # expect(page.all('.merchant')[2]).to have_content('Merchant Name 3, Items Sold: 180')
-          # expect(page.all('.merchant')[3]).to have_content('Merchant Name 4, Items Sold: 170')
-          # expect(page.all('.merchant')[4]).to have_content('Merchant Name 5, Items Sold: 160')
-          # expect(page.all('.merchant')[5]).to have_content('Merchant Name 6, Items Sold: 150')
-          # expect(page.all('.merchant')[6]).to have_content('Merchant Name 7, Items Sold: 140')
-          # expect(page.all('.merchant')[7]).to have_content('Merchant Name 8, Items Sold: 130')
-          # expect(page.all('.merchant')[8]).to have_content('Merchant Name 9, Items Sold: 120')
-          # expect(page.all('.merchant')[9]).to have_content('Merchant Name 10, Items Sold: 110')
+          expect(page.all('.merchant')[0]).to have_content("#{@merchant_1.name}, Orders: 10")
+          expect(page.all('.merchant')[1]).to have_content("#{@merchant_2.name}, Orders: 9")
+          expect(page.all('.merchant')[2]).to have_content("#{@merchant_3.name}, Orders: 8")
+          expect(page.all('.merchant')[3]).to have_content("#{@merchant_4.name}, Orders: 7")
+          expect(page.all('.merchant')[4]).to have_content("#{@merchant_5.name}, Orders: 6")
+          expect(page.all('.merchant')[5]).to have_content("#{@merchant_6.name}, Orders: 5")
+          expect(page.all('.merchant')[6]).to have_content("#{@merchant_7.name}, Orders: 4")
+          expect(page.all('.merchant')[7]).to have_content("#{@merchant_8.name}, Orders: 3")
+          expect(page.all('.merchant')[8]).to have_content("#{@merchant_9.name}, Orders: 2")
+          expect(page.all('.merchant')[9]).to have_content("#{@merchant_10.name}, Orders: 1")
         end
       end
     end
